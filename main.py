ec242db329c125d9b1b438f5711523462fe04c2c
Sebastien <sebastien.crouzet@viacesi.fr>
2022-06-08 20:49:32 +0200

    The 90% finishing project

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import math as math
 import os.path
 from random import *
 import folium
-from ortools.constraint_solver import routing_enums_pb2
-from ortools.constraint_solver import pywrapcp
+from networkx import DiGraph, from_numpy_matrix, relabel_nodes, set_node_attributes
+from vrpy import VehicleRoutingProblem
+from numpy import array
 import matplotlib.pyplot as plt
 import webbrowser
 
@@ -55,57 +56,49 @@ def GenererMatrice(PointTravail):
     MatriceDistances = []
     MatriceLocalisation = []
     for i in range(len(PointTravail)):
-        MatriceDistances.append([])
+        MatriceDistances.append([0])
         MatriceLocalisation.append([])
         for j in range(len(PointTravail)):
             MatriceDistances[i].append(CalculDistanceHaversine(PointTravail[i], PointTravail[j]))
             MatriceLocalisation[i].append(f"{PointTravail[i].Ville}->{PointTravail[j].Ville}")
-
+    MatriceDistances.append([0 for _ in range(len(PointTravail)+1)])
     return MatriceDistances, MatriceLocalisation
 
 
 def Mapping(PointsTravail, nombre_livraison):
     "Mappe les points de livraison"
     PointsLivraison = []
-    Points = PointsTravail
+    DictonaireLivraison = {}
     for _ in range(nombre_livraison):
-        index = randint(0, len(Points)-1)
-        PointsLivraison.append(Points[index])
-        Points.remove(Points[index])
-    return PointsLivraison
+        index = randint(0, len(PointsTravail)-1)
+        PointsLivraison.append(PointsTravail[index])
+        DictonaireLivraison[_] = PointsTravail[index].Ville
+    return DictonaireLivraison, PointsLivraison
 
 
-def genererCarte(ordre, PointLivraison):
+def genererCarte(ordrepasage, PointLivraison):
     "Génère une carte"
     Carte = folium.Map(location=[PointLivraison[0].Latitude, PointLivraison[0].Longitude], zoom_start=10)
     i=1
     for each_item in PointLivraison:
-        folium.Marker(location=[each_item.Latitude, each_item.Longitude], popup=f"{each_item.Ville}({str(ordre[i])})", icon=folium.Icon(color='green')).add_to(Carte)
+        folium.Marker(location=[each_item.Latitude, each_item.Longitude], popup=f"{each_item.Ville}", icon=folium.Icon(color='green')).add_to(Carte)
+        each_item.ordre = i-1
         i=i+1
-    folium.PolyLine([(float(each_item.Latitude), float(each_item.Longitude)) for each_item in PointLivraison], color='red', weight=1).add_to(Carte)
+
+    for each_item in ordrepasage:
+        for indice in range(len(ordrepasage[each_item])):
+            if type(ordrepasage[each_item][indice]) == str:
+                ordrepasage[each_item][indice] = 0
+
+    ListColor = ['red', 'blue', 'green', 'grey', 'orange', 'purple', 'pink', 'brown', 'black', 'grey']
+    for indice in ordrepasage:
+        folium.PolyLine([(float(PointLivraison[each_item].Latitude), float(PointLivraison[each_item].Longitude)) for each_item in ordrepasage[indice]], color=ListColor[indice], weight=2).add_to(Carte)
+
+    
+
     Carte.save(os.path.abspath(os.path.dirname(__file__))+ '\Map.html')
     webbrowser.open('file://' + os.path.realpath('Map.html'))
 
-def print_solution(data, manager, routing, solution):
-    """Prints solution on console."""
-    print(f'Objective: {solution.ObjectiveValue()}')
-    max_route_distance = 0
-    for vehicle_id in range(1):
-        index = routing.Start(vehicle_id)
-        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
-        route_distance = 0
-        while not routing.IsEnd(index):
-            plan_output += ' {} -> '.format(manager.IndexToNode(index))
-            previous_index = index
-            index = solution.Value(routing.NextVar(index))
-            route_distance += routing.GetArcCostForVehicle(
-                previous_index, index, vehicle_id)
-        plan_output += '{}\n'.format(manager.IndexToNode(index))
-        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
-        print(plan_output)
-        max_route_distance = max(route_distance, max_route_distance)
-    print('Maximum of the route distances: {}m'.format(max_route_distance))
-    return plan_output
 
 def main():
     DepartementTravail = randint(1,93)
@@ -113,56 +106,20 @@ def main():
     print(f"Departement de travail : {str(DepartementTravail)}")
     PointsTravail = readfile(DepartementTravail)
     MateriauxSpecial = randint(1, len(PointsTravail)-1)
-    PointLivraison = Mapping(PointsTravail, nombre_livraison)
-    numvehicule = 1
+    DictonaireLivraison, PointLivraison = Mapping(PointsTravail, nombre_livraison)
     print(f"Point de départ : {PointLivraison[0].Ville}")
     MatriceDistances, MatriceLocalisation = GenererMatrice(PointLivraison)
-    # Create the routing index manager.
-    manager = pywrapcp.RoutingIndexManager(len(MatriceDistances),numvehicule, 0)
+    A = array(MatriceDistances, dtype=[("cost", float)])
+    G = from_numpy_matrix(A, create_using=DiGraph())
 
-    # Create Routing Model.
-    routing = pywrapcp.RoutingModel(manager)
+    # The depot is relabeled as Source and Sink
+    G = relabel_nodes(G, {0: "Source", len(G)-1: "Sink"})
 
-    # Create and register a transit callback.
-    def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
-        # Convert from routing variable Index to distance matrix NodeIndex.
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return MatriceDistances[from_node][to_node]
-
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-
-    # Define cost of each arc.
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-
-    # Add Distance constraint.
-    dimension_name = 'Distance'
-    routing.AddDimension(
-        transit_callback_index,
-        0,  # no slack
-        3000000,  # vehicle maximum travel distance
-        True,  # start cumul to zero
-        dimension_name)
-    distance_dimension = routing.GetDimensionOrDie(dimension_name)
-    distance_dimension.SetGlobalSpanCostCoefficient(100)
-
-    # Setting first solution heuristic.
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-
-    # Solve the problem.
-    solution = routing.SolveWithParameters(search_parameters)
-
-    # Print solution on console.
-    if solution:
-        print_solution(MatriceDistances, manager, routing, solution)
-    else:
-        print('No solution found !')
-
-    #GenerateMap
-    genererCarte(str(print_solution(MatriceDistances, manager, routing, solution)).split(" -> "),PointLivraison)
+    prob = VehicleRoutingProblem(G)
+    prob.minimize_global_span = True
+    prob.solve()
+    print(prob.best_routes)
+    genererCarte(prob.best_routes,PointLivraison)
 
 
 main()
